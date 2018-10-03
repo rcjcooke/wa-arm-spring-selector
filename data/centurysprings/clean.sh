07359383d0ad8596a79dab6fcfa4c8470a221d02
Ray Cooke <rcjcooke@gmail.com>
2018-10-03 18:41:52 +1300

    Gradelised data project :)

--- data/centurysprings/clean.sh
+++ data/centurysprings/clean.sh
@@ -4,9 +4,11 @@
 # Functions
 ################
 usage() {
-  echo "usage: ./clean.sh [-e] [-h]"
+  echo "usage: ./clean.sh -o outputFile [-i inputPath] [-e] [-h]"
   echo ""
-  echo -e "-e, --excel\t\tOpen the resulting output file (out.csv) in Excel"
+  echo -e "-o, --output\t\tOutput the result to the specified file, e.g. build/out.csv"
+  echo -e "-i, --input\t\tThe path to the CS.P* input files, e.g. src/main/resources/ - default: ./"
+  echo -e "-e, --excel\t\tOpen the resulting output file in Excel"
   echo -e "-h, --help\t\tShow this usage text"
 }
 
@@ -16,31 +18,50 @@ usage() {
 
 # Setup control variables
 openInExcel=0
+outputFile=""
+inputPath="./"
 
 # Process arguments
 while [ "$1" != "" ]; do
     case $1 in
-        -e | --excel )          openInExcel=1
-                                ;;
-        -h | --help )           usage
-                                exit
-                                ;;
-        * )                     usage
-                                exit 1
+        -i | --input )  shift
+                        inputPath=$1
+                        ;;
+        -o | --output ) shift
+                        outputFile=$1
+                        ;;
+        -e | --excel )  openInExcel=1
+                        ;;
+        -h | --help )   usage
+                        exit
+                        ;;
+        * )             echo "Unexpected argument: $1"
+                        usage
+                        exit 1
     esac
     shift
 done
+if [ "$outputFile" = "" ]; then
+  echo "No output file specified"
+  usage
+  exit 2
+fi
+
+# Make sure the directory exists for the file
+mkdir -p -- "${outputFile%/*}" 
 
 # Put the headers in
-echo "OD / in,OD / mm,Order Num,Length / in,Length / mm,Rate / lbs per in,Rate / N per mm,Initial Tension / lbs,Initial Tension / N,Sugg. Max. Defl. / in,Sugg. Max. Defl. / mm,Sugg. Max. Load / lbs,Sugg. Max. Load / N,Wire Dia. / in,Wire Dia. / mm,MAT'L,FNSH" > out.csv
+echo "OD / in,OD / mm,Order Num,Length / in,Length / mm,Rate / lbs per in,Rate / N per mm,Initial Tension / lbs,Initial Tension / N,Sugg. Max. Defl. / in,Sugg. Max. Defl. / mm,Sugg. Max. Load / lbs,Sugg. Max. Load / N,Wire Dia. / in,Wire Dia. / mm,MAT'L,FNSH" > $outputFile
 # Process all 55 pages
 for ((i=1;i<55;i++)); do
-  echo "`cat out.csv | wc -l`: Processing CS.P$i ..."
-  cat "CS.P$i" | sed 's/  */,/g' | sed 's/,\(MW\|SPR\|HD\|OT\|SST\|BC\|PB\|B\)\([A-Z,]*\),\([0-9]*\)/,\1\2\n\3/g' | ./CSCSVClean.awk >> out.csv
+  echo "`cat $outputFile | wc -l`: Processing $inputPath/CS.P$i ..."
+  cat "$inputPath/CS.P$i" | sed 's/  */,/g' | sed 's/,\(MW\|SPR\|HD\|OT\|SST\|BC\|PB\|B\)\([A-Z,]*\),\([0-9]*\)/,\1\2\n\3/g' | ./CSCSVClean.awk >> $outputFile
 done
-echo "Done: `cat out.csv | wc -l` lines processed"
-# Open it in excel
+echo "Done: `cat $outputFile | wc -l` lines processed"
+echo "Output file: $outputFile"
+
+# Open it in excel if required
 if [ "$openInExcel" = "1" ]; then
-  echo "Opening out.csv for review in Excel..."
-  "C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE" out.csv
+  echo "Opening $outputFile for review in Excel..."
+  "C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE" $outputFile
 fi
--- data/process-raw-csvs.sh
+++ data/process-raw-csvs.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 ################
-# Constants
-################
-OUTPUT_FILE="./processed/processed.csv"
-
-################
 # Functions
 ################
 usage() {
-  echo "usage: ./process-raw-csvs.sh [-e] [-h]"
+  echo "usage: ./process-raw-csvs.sh -s scriptInputPath -c csvInputPath -o outputFile [-e] [-h]"
   echo ""
+  echo -e "-s, --scriptinputpath\t\tThe directory containing the selector scripts, e.g. src/main/scripts"
+  echo -e "-c, --csvinputpath\t\tThe directory containing the CSVs to be processed, e.g. build/csvs"
+  echo -e "-o, --outputfile\t\tOutput the result to the specified file, e.g. build/processed.csv"
   echo -e "-e, --excel\t\tOpen the resulting output file (out.csv) in Excel"
   echo -e "-h, --help\t\tShow this usage text"
 }
@@ -21,32 +19,71 @@ usage() {
 
 # Setup control variables
 openInExcel=0
+scriptInputPath=""
+csvInputPath=""
+outputFile=""
 
 # Process arguments
 while [ "$1" != "" ]; do
     case $1 in
-        -e | --excel )          openInExcel=1
-                                ;;
-        -h | --help )           usage
-                                exit
-                                ;;
-        * )                     usage
-                                exit 1
+        -s | --scriptinputpath )  shift
+                                  scriptInputPath=$1
+                                  ;;
+        -c | --csvinputpath )     shift
+                                  csvInputPath=$1
+                                  ;;
+        -o | --outputfile )       shift
+                                  outputFile=$1
+                                  ;;
+        -e | --excel )            openInExcel=1
+                                  ;;
+        -h | --help )             usage
+                                  exit
+                                  ;;
+        * )                       usage
+                                  exit 1
     esac
     shift
 done
+if [ "$scriptInputPath" = "" ]; then
+  echo "No script input directory specified"
+  usage
+  exit 2
+fi
+if [ "$csvInputPath" = "" ]; then
+  echo "No CSV input directory specified"
+  usage
+  exit 3
+fi
+if [ "$outputFile" = "" ]; then
+  echo "No output file specified"
+  usage
+  exit 4
+fi
 
 # Put the headers in
-echo "Bestellnummer,Hersteller,k[N/mm] Federrate,Masse,Lr[mm] Relevante Laenge,V[Nmm] Maximale potentielle Energie,Fn[N] Hoechstkraft bei statischer Belastung,De[mm] Ausserer Windungsdurchmesser,d[mm] Drahtdurchmesser" > $OUTPUT_FILE
+echo "Bestellnummer,Hersteller,k[N/mm] Federrate,Masse,Lr[mm] Relevante Laenge,V[Nmm] Maximale potentielle Energie,Fn[N] Hoechstkraft bei statischer Belastung,De[mm] Ausserer Windungsdurchmesser,d[mm] Drahtdurchmesser" > $outputFile
 # Process all sources
-echo "Processing Gutekunst ..."
-./gutekunst/process-raw-gk-csv.awk ./raw/Federndatenbank2.csv >> $OUTPUT_FILE
-echo "Processing Century Springs ..."
-./centurysprings/process-raw-cs-csv.awk ./raw/cs.csv >> $OUTPUT_FILE
-echo "Done: `cat $OUTPUT_FILE | wc -l` lines processed"
+for scriptFile in $scriptInputPath/*.awk; do
+  # Skip over if there's no match
+  [ -e "$scriptFile" ] || continue
+  # Get the file name by stripping the path away
+  filename=${scriptFile##*/}
+  # Get the base by stripping away everything after and including the first '-' 
+  base=${filename%%-*}
+  # Execute the script against the CSV file and append it to the output file
+  ./$scriptFile $csvInputPath/$base.csv >> $outputFile
+done
+
+
+# echo "Processing Gutekunst ..."
+# ./gutekunst/process-raw-gk-csv.awk ./raw/Federndatenbank2.csv >> $outputFile
+# echo "Processing Century Springs ..."
+# ./centurysprings/process-raw-cs-csv.awk ./raw/cs.csv >> $outputFile
+# echo "Done: `cat $outputFile | wc -l` lines processed"
 
 # Open it in excel
 if [ "$openInExcel" = "1" ]; then
   echo "Opening out.csv for review in Excel..."
-  "C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE" $OUTPUT_FILE
+  "C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE" $outputFile
 fi
